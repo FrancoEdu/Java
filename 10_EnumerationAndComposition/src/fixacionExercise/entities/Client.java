5249edc276805621b6f19ddd6948c78c3ef2e73c
FrancoEdu <franc0edu24@gmail.com>
2022-12-10 17:41:06 -0300

    Finished exercise

--- 10_EnumerationAndComposition/src/fixacionExercise/entities/Client.java
+++ 10_EnumerationAndComposition/src/fixacionExercise/entities/Client.java
@@ -1,14 +1,15 @@
 package fixacionExercise.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Client {
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
     private String name;
     private String email;
     private Date birthDate;
 
-    public Client() {
-    }
     public Client(String name, String email, Date birthDate) {
         this.name = name;
         this.email = email;
@@ -37,5 +38,10 @@ public class Client {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + sdf.format(birthDate) + ") - " + email;
     }
 }
