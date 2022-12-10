25bc61e436377f5986c435f031f760ba0483d8f4
FrancoEdu <franc0edu24@gmail.com>
2022-12-10 14:11:22 -0300

    First exercise using teachings enumerations and compositions

---
+++ 10_EnumerationAndComposition/src/FirstExercise/application/Main.java
@@ -0,0 +1,7 @@
+package FirstExercise.application;
+
+public class Main {
+    public static void main(String[] arg){
+
+    }
+}
---
+++ 10_EnumerationAndComposition/src/FirstExercise/entities/WorkerLevel.java
@@ -0,0 +1,5 @@
+package FirstExercise.entities;
+
+public enum WorkerLevel {
+    JUNIOR,MID_LEVEL,SENIOR;
+}
