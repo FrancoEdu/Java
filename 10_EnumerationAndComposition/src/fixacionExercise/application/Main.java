32b83399e5dcb739a6e5090654b33f19c1cca691
FrancoEdu <franc0edu24@gmail.com>
2022-12-10 16:46:03 -0300

    Exercise to traine the concepts

---
+++ 10_EnumerationAndComposition/src/fixacionExercise/application/Main.java
@@ -0,0 +1,9 @@
+package fixacionExercise.application;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg){
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ 10_EnumerationAndComposition/src/fixacionExercise/entities/OrderStatus.java
@@ -0,0 +1,8 @@
+package fixacionExercise.entities;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
