0905e3a5883d9755ed16310976d93ecd0aa1d28b
FrancoEdu <franc0edu24@gmail.com>
2022-12-20 15:34:32 -0300

    Created subclass businessAccount

--- _11 - heranceAndPolimorfism/src/entities/BusinessAccount.java
+++ _11 - heranceAndPolimorfism/src/entities/BusinessAccount.java
@@ -2,7 +2,27 @@ package entities;
 
 public class BusinessAccount extends Account{
     private Double loanlimit;
-    public BusinessAccount(Integer number, String holder, Double balance) {
-        super(number, holder, balance);
+
+    public BusinessAccount(){
+        super();
     }
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanlimit){
+        super(number,holder,balance);
+        this.loanlimit = loanlimit;
+    }
+
+    public void setLoanlimit(Double loanlimit){
+        this.loanlimit = loanlimit;
+    }
+
+    public Double getLoanlimit(){
+        return this.loanlimit;
+    }
+
+    public void loan(Double amount){
+        if (amount <= loanlimit){
+            balance = balance + amount - 10;
+        }
+    }
+
 }
