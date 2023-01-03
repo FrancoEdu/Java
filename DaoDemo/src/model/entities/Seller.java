057d2a46e6cb83899fe7a094da386ee075ec37c8
FrancoEdu <franc0edu24@gmail.com>
2023-01-03 17:49:06 -0300

    Created class Seller

---
+++ DaoDemo/src/model/entities/Seller.java
@@ -0,0 +1,12 @@
+package model.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private Double baseSalary;
+}
