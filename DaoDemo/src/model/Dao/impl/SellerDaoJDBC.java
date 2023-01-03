bf4f1d8b5ef03d5fa02d53dc0ba40e8a5c1b2e51
FrancoEdu <franc0edu24@gmail.com>
2023-01-03 18:20:15 -0300

    SellerDaoJDBC and DaoFactory

---
+++ DaoDemo/src/model/Dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,27 @@
+package model.Dao.impl;
+
+import model.Dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(SellerDao obj) {
+
+    }
+
+    @Override
+    public void update(SellerDao obj) {
+
+    }
+
+    @Override
+    public void deleteById(SellerDao id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+}
