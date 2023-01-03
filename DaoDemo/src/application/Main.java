c06352b5cf94075f675e7d90cebeac9525a7e1da
FrancoEdu <franc0edu24@gmail.com>
2023-01-03 18:03:44 -0300

    Seller entity class

--- DaoDemo/src/application/Main.java
+++ DaoDemo/src/application/Main.java
@@ -1,5 +1,16 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Department obj = new Department(1,"Computers");
+        Seller seller = new Seller(21,"Edu","eduardo.franco@cnpem.br",new Date(), 3000.0, obj);
+        System.out.println(obj + "\n" + seller);
     }
 }
